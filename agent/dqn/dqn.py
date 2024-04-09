38d9ebb7864facd96f3a049482204b5a0d02cfb0
Taewoon Kim <tae898@gmail.com>
2024-04-09 14:54:34 +0200

    WIP

--- agent/dqn/dqn.py
+++ agent/dqn/dqn.py
@@ -301,55 +301,6 @@ class DQNAgent:
             for policy_type in ["mm", "explore", "qa"]:
                 transitions[policy_type] = []
 
-        if self.agent_to_episodic:
-            agent_location_observations = []
-            remaining_room_observations = []
-
-            for obs in observations["room"]:
-                if obs[0] == "agent" and obs[1] == "atlocation":
-                    agent_location_observations.append(obs)
-                else:
-                    remaining_room_observations.append(obs)
-
-            if len(agent_location_observations) > 0:
-                for obs in agent_location_observations:
-                    encode_observation(self.memory_systems, obs)
-                    manage_memory(
-                        self.memory_systems,
-                        "episodic",
-                        split_possessive=False,
-                    )
-        else:
-            remaining_room_observations = observations["room"]
-
-        encode_all_observations(self.memory_systems, remaining_room_observations)
-
-        # Memory management policy. There are multiple agents for this policy.
-        num_mm_agents = len(remaining_room_observations)
-        working_memory = self.get_working_memory()
-
-        for idx in range(num_mm_agents):
-            state = deepcopy(working_memory) + idx
-
-            action, q_values_ = select_action(
-                state=state,
-                greedy=greedy,
-                gnn=self.gnn,
-                dqn=self.dqn["mm"],
-                epsilon=self.epsilon,
-                action_space=self.action_space["mm"],
-            )
-            states["mm"].append(state)
-            actions["mm"].append(action)
-            q_values["mm"].append(q_values_)
-
-        for state, action in zip(states["mm"], actions["mm"]):
-            manage_memory(
-                self.memory_systems,
-                self.action2str["mm"][action],
-                split_possessive=False,
-            )
-
         long_term_memory = self.get_long_term_memory()
 
         # Question answering policy. There is only one agent for this policy.
@@ -399,6 +350,64 @@ class DQNAgent:
         next_states["qa"] = None
         next_states["explore"] = None
 
+    def step_1(self, obsevations_room: list, greedy: bool) -> None:
+        """Run one step of the agent.
+
+        Args:
+            obsevations_room: the observations from the environment
+            greedy: whether to act greedily
+
+        """
+        states_mm = []
+        actions_mm = []
+
+        next_states_mm = []
+
+        if self.agent_to_episodic:
+            agent_location_observations = []
+            remaining_room_observations = []
+
+            for obs in obsevations_room:
+                if obs[0] == "agent" and obs[1] == "atlocation":
+                    agent_location_observations.append(obs)
+                else:
+                    remaining_room_observations.append(obs)
+
+            if len(agent_location_observations) > 0:
+                for obs in agent_location_observations:
+                    encode_observation(self.memory_systems, obs)
+                    manage_memory(
+                        self.memory_systems,
+                        "episodic",
+                        split_possessive=False,
+                    )
+        else:
+            remaining_room_observations = obsevations_room["room"]
+
+        encode_all_observations(self.memory_systems, remaining_room_observations)
+
+        # Memory management policy. There are multiple agents for this policy.
+        num_mm_agents = len(remaining_room_observations)
+        working_memory = self.get_working_memory()
+
+        for idx in range(num_mm_agents):
+            state = deepcopy(working_memory) + idx
+
+            action, q_values_ = select_action(
+                state=state,
+                greedy=greedy,
+                gnn=self.gnn,
+                dqn=self.dqn_mm,
+                epsilon=self.epsilon,
+                action_space=self.action_space_mm,
+            )
+        for state, action in zip(states["mm"], actions["mm"]):
+            manage_memory(
+                self.memory_systems,
+                self.action2str["mm"][action],
+                split_possessive=False,
+            )
+
     def fill_replay_buffer(self) -> None:
         """Make the replay buffer full in the beginning with the uniformly-sampled
         actions. The filling continues until it reaches the warm start size.
@@ -412,6 +421,9 @@ class DQNAgent:
                 observations, info = self.env.reset()
                 done = False
                 new_episode_starts = False
+
+                transitions_mm = []
+                transitions_explore = []
 
             observations, action, reward, done, q_values = self.step(
                 observations, greedy=False, save_to_replay_buffer=False
